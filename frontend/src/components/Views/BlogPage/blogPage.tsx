4c448d4a2c4c6f655883617233bacef9312fafa9
yanir21 <yanir334@gmail.com>
2022-12-31 17:36:23 +0200

    feat(blogPage.tsx): search bar

--- frontend/src/components/Views/BlogPage/blogPage.tsx
+++ frontend/src/components/Views/BlogPage/blogPage.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Login from "../Login/login";
 import "./blogPage.css";
-import { Button } from "react-bootstrap";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
 import PostCard from "../../PostCard/postCard";
 import { useQuery } from "@tanstack/react-query";
 import { fetchPosts } from "../../../services/post";
 import CreatePostModal from "../../CreatePostModal/createPostModal";
 import { Post } from "../../../models/post";
 import { promptError, promptSuccess } from "../../../services/toast";
+import { Search } from "react-bootstrap-icons";
 
 const BlogPage = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [editedPost, setEditedPost] = useState<Post>();
+  const [search, setSearch] = useState<string>("");
+  const [searchField, setSearchField] = useState<keyof Post>("title");
   const { isLoading, data, refetch } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
@@ -22,11 +25,26 @@ const BlogPage = () => {
     setShowModal(false);
   };
 
+  const filteredPosts = useMemo(
+    () => data?.filter((post) => post[searchField].toString().includes(search)),
+    [data, search, Search]
+  );
+
   return (
     <div className="blog-page">
       <div className="top-row">
         <span className="top-title">Recent Posts</span>
-
+        <InputGroup className="input-group">
+          <InputGroup.Text>
+            <Search />
+          </InputGroup.Text>
+          <Form.Control
+            type="text"
+            placeholder={`Search by ${searchField}`}
+            value={search}
+            onChange={({ target }) => setSearch(target.value)}
+          />
+        </InputGroup>
         <span>
           <Button onClick={setShowModal.bind(this, true)}>
             Create New Post
@@ -36,10 +54,10 @@ const BlogPage = () => {
       <div className="posts-container">
         {isLoading ? (
           <>Loading Your Data...</>
-        ) : !data || data.length === 0 ? (
+        ) : !filteredPosts || filteredPosts.length === 0 ? (
           <>No Posts to show</>
         ) : (
-          data.map((blogPost) => (
+          filteredPosts.map((blogPost) => (
             <PostCard
               key={blogPost._id}
               post={blogPost}
